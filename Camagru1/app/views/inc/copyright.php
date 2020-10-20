1a8adbedaa3b8a1b5ab46c7408b7a6d9e49bea39
hassanreus <hassanseffiani@gmail.com>
2020-10-20 13:30:10 +0100

    3

--- Camagru1/app/views/inc/copyright.php
+++
@@ -1,7 +0,0 @@
-<footer style="position: fixed;left: 0;bottom: 0;width: 100%;background-color: #DDDDDD;color: white;text-align: center;">
-        <div class="content has-text-centered">
-            <p>
-                <strong>Camagru</strong> by &copy;<strong>hseffian</strong>
-            </p>
-        </div>
-</footer>
--- Camagru1/app/views/inc/footer.php
+++
@@ -1,6 +0,0 @@
-    <?php require APPROOT . '/views/inc/copyright.php'?>
-    <script src="<?php echo URLROOT; ?>/js/main.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
-</body>
-</html>
